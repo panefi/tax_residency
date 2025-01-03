ba6355000b5ecd7140b49284f0aa992b167a03a1
Efi Panagopoulou <efipanagopoulou@Efis-MacBook-Air.local>
2025-01-03 22:14:33 +0200

    fix: Add domain

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 from routes.users import users_router
 from routes.travel import travel_router
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = FastAPI()
 
@@ -10,4 +13,4 @@ app.include_router(users_router, prefix='/api/v1/users')
 app.include_router(travel_router, prefix='/api/v1/travel')
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host=os.getenv('HOST'), port=8000)
